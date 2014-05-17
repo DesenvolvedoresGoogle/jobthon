aaafd7bfd70aec8f4b406fa888e542ecb0859ef1
Nassor Paulino da Silva <nassor@gmail.com>
2014-05-16 22:16:49 -0300

    endpoints vagas

--- backend/jobthon.go
+++ backend/jobthon.go
@@ -2,59 +2,17 @@ package gdgjobthon
 
 import (
 	"encoding/json"
+	"errors"
 	"log"
 	"net/http"
-
-	"github.com/go-martini/martini"
-	"github.com/martini-contrib/render"
+	"strconv"
 
 	"appengine"
 	"appengine/datastore"
+
+	"github.com/go-martini/martini"
+	"github.com/martini-contrib/render"
 )
-
-func AppEngine(c martini.Context, r *http.Request) {
-	c.Map(appengine.NewContext(r))
-}
-
-func CORS(w http.ResponseWriter) {
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-}
-
-func init() {
-	m := martini.Classic()
-	m.Use(render.Renderer())
-	m.Use(AppEngine)
-	m.Use(CORS)
-
-	m.Get("/empresas", listEmpresas)
-	m.Get("/empresa/:email", getEmpresa)
-	m.Post("/empresa", addEmpresa)
-
-	m.Get("/vagas", listaVagas)
-	m.Get("/vaga/:email", getVaga)
-
-	m.Get("/curriculo/:email", getCurriculo)
-	m.Post("/curriculo", addCurriculo)
-
-	// curriculosFake := []Curriculo{
-	// 	Curriculo{
-	// 		Nome:        "Ricardo Cherobin",
-	// 		Idade:       27,
-	// 		Cidade:      "Florianópolis",
-	// 		Estado:      "SC",
-	// 		Habilidades: []string{"PHP", "Java", "MySQL", "IA"},
-	// 	},
-	// 	Curriculo{
-	// 		Nome:        "Nassor Paulino da Silva",
-	// 		Idade:       29,
-	// 		Cidade:      "Florianópolis",
-	// 		Estado:      "SC",
-	// 		Habilidades: []string{"PHP", "Java", "MySQL", "Postgres", "Go", "Python", "Ruby", "Ruby on Rails", "SolR", "Elasticsearch"},
-	// 	},
-	// }
-
-	http.Handle("/", m)
-}
 
 type Analise struct {
 	Id              int64 `json:"id" datastore:"-"`
@@ -64,10 +22,17 @@ type Analise struct {
 	Compatibilidade float64
 }
 
-// Empresa
-// Empresa Model
+type Curriculo struct {
+	Email       string   `json:"email"`
+	Nome        string   `json:"nome"`
+	Idade       int64    `json:"idade"`
+	Habilidades []string `json:"habilidades"`
+	Cidade      string   `json:"cidade"`
+	Estado      string   `json:"estado"`
+	Telefone    string   `json:"telefone"`
+}
+
 type Empresa struct {
-	Id       int64  `json:"id" datastore:"-"`
 	Email    string `json:"email"`
 	Telefone string `json:"telefone"`
 	Nome     string `json:"nome"`
@@ -76,28 +41,61 @@ type Empresa struct {
 	Sobre    string `json:"sobre"`
 }
 
-// Empresa Controllers
-func listEmpresas(r render.Render) {
-	empresasFake := []Empresa{
-		Empresa{
-			Nome:     "Dev Coop",
-			Email:    "dev@coop.com.br",
-			Cidade:   "São Paulo",
-			Estado:   "SP",
-			Telefone: "4844444444",
-			Sobre:    "Mauris iaculis est a vestibulum venenatis. Vestibulum justo lacus, aliquet sit amet est at, laoreet tincidunt sapien. Nunc ac lectus ultrices, iaculis mauris non, commodo tellus. Nullam dictum eleifend molestie. Integer malesuada turpis non sem hendrerit aliquet. Proin id convallis turpis. Mauris ornare id nibh vel rhoncus. Aliquam tincidunt nunc in vehicula mollis. Suspendisse posuere eros id augue congue viverra.",
-		},
-		Empresa{
-			Nome:     "Hackthon Dev Team",
-			Email:    "hackthondev@team.com.br",
-			Cidade:   "Florianópolis",
-			Estado:   "SC",
-			Telefone: "4833333333",
-			Sobre:    "É nois no POG: Do mesmo modo, o consenso sobre a necessidade de qualificação desafia a capacidade de equalização das direções preferenciais no sentido do progresso.",
-		},
+type Vaga struct {
+	Id          int64    `json:"id"`
+	Email       string   `json:"email"`
+	Titulo      string   `json:"titulo"`
+	Sobre       string   `json:"sobre"`
+	Habilidades []string `json:"habilidades"`
+	Area        string   `json:"area"`
+	Cidade      string   `json:"cidade"`
+	Estado      string   `json:"estado"`
+	Ativa       bool     `json:"ativa"`
+	Contratacao []string `json:"contratacao"`
+}
+
+func appEngine(c martini.Context, r *http.Request) {
+	c.Map(appengine.NewContext(r))
+}
+
+func cORS(w http.ResponseWriter) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+}
+
+func init() {
+	m := martini.Classic()
+	m.Use(render.Renderer())
+	m.Use(appEngine)
+	m.Use(cORS)
+
+	m.Get("/empresas", listEmpresas)
+	m.Get("/empresa/:email", getEmpresa)
+	m.Post("/empresa", addEmpresa)
+
+	m.Get("/vagas", listVagas)
+	m.Get("/vagas/:email", getVagas)
+	m.Get("/vaga/:id", getVaga)
+	m.Post("/vaga", addVaga)
+
+	m.Get("/curriculos", listCurriculos)
+	m.Get("/curriculo/:email", getCurriculo)
+	m.Post("/curriculo", addCurriculo)
+
+	http.Handle("/", m)
+}
+
+func listEmpresas(c appengine.Context, r render.Render, params martini.Params) {
+	var empresas []Empresa
+	q := datastore.NewQuery("Empresa")
+	_, err := q.GetAll(c, &empresas)
+
+	if err != nil {
+		log.Println(err)
+		r.JSON(http.StatusInternalServerError, err.Error())
+		return
 	}
 
-	r.JSON(http.StatusOK, empresasFake)
+	r.JSON(http.StatusOK, empresas)
 }
 
 func addEmpresa(c appengine.Context, r render.Render, req *http.Request) {
@@ -123,76 +121,99 @@ func getEmpresa(c appengine.Context, r render.Render, params martini.Params) {
 	if err != nil {
 		log.Println(err)
 		r.JSON(http.StatusInternalServerError, err.Error())
+		return
 	}
 
 	r.JSON(http.StatusOK, empresa)
 }
 
-type Vaga struct {
-	Id          int64    `json:"id" datastore:"-"`
-	Email       string   `json:"email"`
-	Titulo      string   `json:"titulo"`
-	Sobre       string   `json:"sobre"`
-	Habilidades []string `json:"habilidades"`
-	Area        string   `json:"area"`
-	Cidade      string   `json:"cidade"`
-	Estado      string   `json:"estado"`
-	Ativa       bool     `json:"ativa"`
-	Contratacao []string `json:"contratacao"`
-}
-
-func listaVagas(r render.Render) {
-	vagasFake := []Vaga{
-		Vaga{
-			Titulo:      "Programador Go",
-			Sobre:       "É claro que a revolução dos costumes apresenta tendências no sentido de aprovar a manutenção de alternativas às soluções ortodoxas.",
-			Habilidades: []string{"Go", "GAE", "Google Datastore"},
-			Area:        "Desenvolvimento",
-			Cidade:      "Florianópolis",
-			Estado:      "SC",
-			Ativa:       true,
-			Contratacao: []string{"Estágio", "PJ"},
-		},
-		Vaga{
-			Titulo:      "Frontend Developer Angular.js",
-			Sobre:       "Por outro lado, a consulta aos diversos militantes faz parte de um processo de gerenciamento das posturas dos órgãos dirigentes com relação às suas atribuições.",
-			Habilidades: []string{"Javascript", "Angular.js", "HTML", "CSS", "Bootstrap"},
-			Area:        "Design",
-			Cidade:      "Criciúma",
-			Estado:      "SC",
-			Ativa:       true,
-			Contratacao: []string{"Estágio", "PJ", "CLT"},
-		},
-		Vaga{
-			Titulo:      "Desenvolvedor Android",
-			Sobre:       "Caros amigos, o acompanhamento das preferências de consumo cumpre um papel essencial na formulação das diversas correntes de pensamento.",
-			Habilidades: []string{"Java", "Android", "BDD"},
-			Area:        "Mobile",
-			Cidade:      "Curitiba",
-			Estado:      "PR",
-			Ativa:       true,
-			Contratacao: []string{"Estágio", "PJ", "CLT"},
-		},
-		// Stripper
+func addVaga(c appengine.Context, r render.Render, req *http.Request) {
+	// Entender como utilizar as ancestors para linkas as chaves com a empresa
+	// Código abaixo com problemas
+	vaga := new(Vaga)
+	decoder := json.NewDecoder(req.Body)
+	err := decoder.Decode(&vaga)
+	if err != nil {
+		log.Println(err)
+		r.JSON(http.StatusInternalServerError, err.Error())
+		return
 	}
 
-	r.JSON(http.StatusOK, vagasFake)
+	q := datastore.NewQuery("Vaga").Filter("Email =", vaga.Email).Filter("Titulo =", vaga.Titulo)
+	size, err := q.Count(c)
+	if err != nil {
+		log.Println(err)
+		r.JSON(http.StatusInternalServerError, err.Error())
+		return
+	}
+	if size > 0 {
+		err = errors.New("job: já há uma vaga desta empresa cadastrada com este nome")
+		log.Println(err)
+		r.JSON(http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	key := datastore.NewKey(c, "Vaga", "", 0, nil)
+	datastore.Put(c, key, vaga)
+	r.JSON(http.StatusOK, "success")
 }
 
-func getVaga() {
+func getVaga(c appengine.Context, r render.Render, params martini.Params) {
+	intKey, err := strconv.ParseInt(params["id"], 10, 64)
+	if err != nil {
+		log.Println(err)
+		r.JSON(http.StatusInternalServerError, err.Error())
+		return
+	}
 
+	key := datastore.NewKey(c, "Vaga", "", intKey, nil)
+	vaga := new(Vaga)
+	err = datastore.Get(c, key, vaga)
+	if err != nil {
+		log.Println(err)
+		r.JSON(http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	vaga.Id = key.IntID()
+
+	r.JSON(http.StatusOK, vaga)
 }
 
-// Curriculo
-type Curriculo struct {
-	Id          int64    `json:"id" datastore:"-"`
-	Email       string   `json:"email"`
-	Nome        string   `json:"nome"`
-	Idade       int64    `json:"idade"`
-	Habilidades []string `json:"habilidades"`
-	Cidade      string   `json:"cidade"`
-	Estado      string   `json:"estado"`
-	Telefone    string   `json:"telefone"`
+func getVagas(c appengine.Context, r render.Render, params martini.Params) {
+	var vagas []Vaga
+	q := datastore.NewQuery("Vaga").Filter("Email =", params["email"])
+	keys, err := q.GetAll(c, &vagas)
+
+	for idx, _ := range vagas {
+		vagas[idx].Id = keys[idx].IntID()
+	}
+
+	if err != nil {
+		log.Println(err)
+		r.JSON(http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	r.JSON(http.StatusOK, vagas)
+}
+
+func listVagas(c appengine.Context, r render.Render, params martini.Params) {
+	var vagas []Vaga
+	q := datastore.NewQuery("Vaga")
+	keys, err := q.GetAll(c, &vagas)
+
+	for idx, _ := range vagas {
+		vagas[idx].Id = keys[idx].IntID()
+	}
+
+	if err != nil {
+		log.Println(err)
+		r.JSON(http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	r.JSON(http.StatusOK, vagas)
 }
 
 func addCurriculo(c appengine.Context, r render.Render, req *http.Request) {
@@ -218,7 +239,21 @@ func getCurriculo(c appengine.Context, r render.Render, params martini.Params) {
 	if err != nil {
 		log.Println(err)
 		r.JSON(http.StatusInternalServerError, err.Error())
+		return
 	}
 
 	r.JSON(http.StatusOK, curriculo)
+}
+
+func listCurriculos(c appengine.Context, r render.Render, params martini.Params) {
+	var curriculos []Curriculo
+	q := datastore.NewQuery("Curriculo")
+	_, err := q.GetAll(c, &curriculos)
+	if err != nil {
+		log.Println(err)
+		r.JSON(http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	r.JSON(http.StatusOK, curriculos)
 }
